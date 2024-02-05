ba222b69362a74e675de0ba49ad850a392eb248e
nightfury99 <32232422+nightfury99@users.noreply.github.com>
2024-02-05 11:45:54 +0800

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -12,9 +13,17 @@ class HomeController extends AbstractController
      * @Route("/home", name="app_home")
      */
     public function index() {
-
+        $request = Request::createFromGlobals();
+        $name = !empty($request->query->get("name")) ? $request->query->get("name") : "Anonymous";
+        $time = date("h:i:sa");
+        $templateString = "Hi $name! 👋 Welcome to my blog ($time 🕰️)";
+        
+        $twig = $this->get('twig');
+        $template = twig_template_from_string($twig, $templateString);
+        $greeting = $template->render();
+        echo $greeting;
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'greeting'  =>  html_entity_decode($greeting)
         ]);
     }
 }
